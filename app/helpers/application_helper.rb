dd9e341f8189c7deb7066e38e89313702cbd72ee
kw7oe <choongkwern@hotmail.com>
2017-05-12 19:28:42 +0800

    Style first part of Home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
 
+  # View Helper: Brand Related
   def platform_name
     "Selist"
+  end
+
+  def platform_description
+    "Sync with your course, get more done"
   end
   
 end
