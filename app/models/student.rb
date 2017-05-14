83cc4f7303ce9b27e69c8bd2e7bb18aaf532dce0
kw7oe <choongkwern@hotmail.com>
2017-05-14 20:26:09 +0800

    Add back StudentsController, modify routes path & etc

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < User
-    has_many :lists, :through=> :subjects
-    has_many :tasks, :through=> :lists
-    has_many :task_statuses, :through=>:tasks
+    has_many :lists, :through => :subjects
+    has_many :tasks, :through => :lists    
+    has_many :task_statuses, :through =>:tasks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  get 'homepage/:id', to:'student#home'
-  put 'task/done/:userId/:taskId',to:'student#done'
-  get 'subject/:id/:userId', to:'subject#home'
+  get 'students/:id', to:'students#show'
+  put 'task/done/:user_id/:task_id',to:'students#done'
+  get 'subject/:id/:user_id', to:'subject#home'
   root "static_pages#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
